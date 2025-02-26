05cd01d866bc04f679a4cd993b4985285939db25
carlomagno-ale <carlomagno.ale@gmail.com>
2025-02-26 18:12:50 +0100

    add snack 8

--- snack-8/script.js
+++ snack-8/script.js
@@ -21,3 +21,22 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+//1
+
+for(let i = 0; i < students.length; i++){
+  const thisStudent = students[i].name
+  if (thisStudent === 'Marco Lanci'){
+    console.log(`la classe di Marco Lanci è la ${students[i].class}`)
+  }
+}
+
+//2
+const classStudent = students.find(checkStudent)
+
+function checkStudent(student){
+const thisStudent = student.name
+if(thisStudent === 'Marco Lanci'){
+  console.log(`la classe di Marco Lanci è la ${student.class}`)
+}
+}
