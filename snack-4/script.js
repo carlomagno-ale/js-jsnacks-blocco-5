7167998ce49d97b4e1503103532643e5ce96389f
carlomagno-ale <carlomagno.ale@gmail.com>
2025-02-26 16:35:52 +0100

    add snack4

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,22 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+console.log(posts)
+
+//1
+const allAuthor = []
+
+for(let i = 0; i < posts.length; i++){
+  const onlyAuthor = posts[i].author
+  allAuthor.push(onlyAuthor)
+}
+
+console.log(allAuthor)
+
+//2
+const authorOnly = posts.map((name)=>{
+  return name.author
+})
+
+console.log(authorOnly)
