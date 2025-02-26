cda7e1902fbe582cc560a104630090da75e866cd
carlomagno-ale <carlomagno.ale@gmail.com>
2025-02-26 15:23:10 +0100

    add snack 1

--- snack-1/script.js
+++ snack-1/script.js
@@ -2,3 +2,23 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+console.log(names)
+
+//1
+for (let i = 0; i< names.length; i++){
+    const thisName = names[i]
+    console.log(thisName)
+}
+
+//2
+names.forEach(getNames)
+
+function getNames(name){
+    console.log(name)
+}
+
+//3
+let allNames = names.join()
+console.log(allNames)
+
